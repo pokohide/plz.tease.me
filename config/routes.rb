0b8269ef297c4f2a0744a5f9cb560f0d5bb53a84
pokohide <hyde31415@gmail.com>
2017-07-12 11:28:34 +0900

    create root controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  get 'root/index'
+
+  get 'root/about'
+
+  get 'root/contact'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
