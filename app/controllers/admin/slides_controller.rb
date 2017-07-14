ae77028abb98065bb5d0d1c79f00254957ec234e
pokohide <hyde31415@gmail.com>
2017-07-14 09:15:42 +0900

    画像生成完了！！

--- app/controllers/admin/slides_controller.rb
+++ app/controllers/admin/slides_controller.rb
@@ -12,9 +12,20 @@ class Admin::SlidesController < ApplicationController
   end
 
   def create
+    # slide = current_user.slides.create!(create_slide_params)
+    # Ppt2pdfJob.perform_later(slide)
+
+    slide = current_user.slides.new(create_slide_params)
+    original_filename = create_slide_params[:original_file]
+
     binding.pry
-    slide = current_user.slides.create!(create_slide_params)
-    Ppt2pdfJob.perform_later(slide)
+
+    slide.with_lock do
+      slide.slide_outline = pdf2outline(original_filename)
+      slide.image_file = pdf2png(original_filename)
+      slide.save!
+    end
+
     redirect_to edit_admin_slide_path(slide)
   end
 
@@ -48,6 +59,23 @@ class Admin::SlidesController < ApplicationController
   end
 
   private
+
+  def pdf2outline file
+    pdf_file_path = file.path
+    # pdf -> txt に xpdf を使う
+    text = `pdftotext -nopgbrk #{pdf_file_path} -`
+    SlideOutline.new(body: text)
+  end
+
+  def pdf2png file
+    pdf_file_path = file.path
+    Dir.mktmpdir do |dir|
+      png_basename = File.basename(pdf_file_path = file.path, '.*')
+      # pdf の 1 ページ目のみを png に変換
+      `pdftocairo -png -singlefile #{pdf_file_path} #{dir}/#{png_basename}`
+      return File.open("#{dir}/#{png_basename}.png")
+    end
+  end
 
   def create_slide_params
     title = File.basename(params[:slide][:original_file].original_filename, '.*')
--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -3,11 +3,12 @@ class ImageUploader < CarrierWave::Uploader::Base
   include CarrierWave::RMagick
   resize_to_fit(250, "") # 横250px にリサイズ
 
-  if Rails.env.development? || Rails.env.test?
-    storage :file
-  else
-    storage :fog
-  end
+  # if Rails.env.development? || Rails.env.test?
+  #   storage :file
+  # else
+  #   storage :fog
+  # end
+  storage :fog
 
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
