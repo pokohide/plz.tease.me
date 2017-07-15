0c4621d8fe4e47ced3195b887db42c0e6966d659
pokohide <hyde31415@gmail.com>
2017-07-15 16:05:17 +0900

    reply button

--- app/assets/javascripts/components/slides/show.js
+++ app/assets/javascripts/components/slides/show.js
@@ -1,3 +1,10 @@
 $('.slides.show').ready(() => {
-
+  $('.comments').on('click', '.reply', function(e) {
+    e.preventDefault()
+    const username = $(this).attr('data-user')
+    let value = $('#comment_body').val()
+    value = `@${username}　` + value
+    $('#comment_body').val(value)
+    $('#comment_body').focus()
+  })
 })
