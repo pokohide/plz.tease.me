73370238ffd20ed332ea9e88aca100425ae2b052
pokohide <hyde31415@gmail.com>
2017-07-15 16:46:39 +0900

    タブ遷移

--- app/assets/javascripts/components/slides/show.js
+++ app/assets/javascripts/components/slides/show.js
@@ -7,4 +7,15 @@ $('.slides.show').ready(() => {
     $('#comment_body').val(value)
     $('#comment_body').focus()
   })
+
+  $('#tabs').on('click', '.item', function(e) {
+    e.preventDefault()
+    const target = $(this).attr('data-tab')
+    $('#tabs .item.active').removeClass('active')
+    $(this).addClass('active')
+
+    const tabs = ['.comment-tab', '.likes-tab' ,'.statistics-tab', '.note-tab']
+    for(let i = 0; i < tabs.length; i++) $(tabs[i]).hide()
+    $(`.${target}-tab`).fadeIn(500)
+  })
 })
