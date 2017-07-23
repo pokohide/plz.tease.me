2210e69eb860515d711b8148737a3697b7f1c9c3
pokohide <hyde31415@gmail.com>
2017-07-24 04:42:59 +0900

    meta tagsを設定

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -34,6 +34,50 @@ module ApplicationHelper
     "field#{object.errors[attribute].empty? ? '' : ' error'}"
   end
 
+  def show_meta_tags
+    if display_meta_tags.blank?
+      assign_meta_tags
+    end
+    display_meta_tags
+  end
+
+  def assign_meta_tags(options = {})
+    defaults = t('meta_tags.defaults')
+    options.reverse_merge!(defaults)
+
+    site = options[:site]
+    title = options[:title]
+    description = options[:description]
+    keywords = options[:keywords]
+    image = options[:image].presence || image_url('noimage.png')
+
+    configs = {
+      separator: '|',
+      reverse: true,
+      site: options[:site],
+      title: options[:title],
+      description: options[:description],
+      keywords: options[:keywords],
+      canonical: request.original_url,
+      og: {
+        type: 'article',
+        title: title.presence || site,
+        description: description,
+        url: request.original_url,
+        image: image,
+        site_name: site
+      },
+      fb: {
+        app_id: '*****'
+      },
+      twitter: {
+        site: '@twitter_account',
+        card: 'summary',
+      }
+    }
+    set_meta_tags(configs)
+  end
+
   private
 
   def flash_class(level)
